31c520181e1dfee9b0fe9514b37bdd0353af33f4
daijja <buiminh8690@gmail.com>
2023-05-23 10:40:15 +0700

    up

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -16,7 +16,10 @@ const List = (props: PropsType) => {
     const navigate = useNavigate();
     const deleteTask = (e: number) => {
         axios.delete("http://localhost:3000/list/" + e)
-            .then(res => setTogle(!togle))
+            .then(res => {
+                setTogle(!togle);
+                console.log(res);
+            })
             .catch(err => console.log(err));
     }
     const endTask = (item: DataProps) => {
@@ -36,15 +39,15 @@ const List = (props: PropsType) => {
         <div className="mx-80 text-center">
             {data.length > 0 && data.map((item, i) => (
                 <div key={i}>
-                    <div style={item.completed ? {backgroundColor:"silver"} : {}} className="flex justify-between my-1 bg-yellow-200 px-5 py-2 rounded-lg">
-                        <div style={item.completed ? {textDecoration: "line-through"} : {}}>
+                    <div style={item.completed ? { backgroundColor: "silver" } : {}} className="flex justify-between my-1 bg-yellow-200 px-5 py-2 rounded-lg">
+                        <div style={item.completed ? { textDecoration: "line-through" } : {}}>
                             <div className="text-3xl font-semibold">{item.title}</div>
                             <div className="text-start text-sm">{item.description}</div>
                         </div>
                         <div className="flex">
                             <button className="px-5 py-1 mx-1 border-2 m-auto hover:bg-red-600 hover:text-white" onClick={() => deleteTask(item.id)}><i className="fa-solid fa-trash-can"></i></button>
-                            <button style={item.completed ? {display: "none"}:{}} className="px-5 py-1 mx-1 border-2 m-auto" onClick={() => navigate(`/${item.id}`)}><i className="fa-solid fa-pen-to-square"></i></button>
-                            <button style={item.completed ? {backgroundColor: "green"} : {}} className="px-5 py-1 mx-1 border-2 m-auto hover:bg-green-600 " onClick={() => endTask(item)}>{item.completed ? "Completed" : "Complete"}</button>
+                            <button style={item.completed ? { display: "none" } : {}} className="px-5 py-1 mx-1 border-2 m-auto" onClick={() => navigate(`/${item.id}`)}><i className="fa-solid fa-pen-to-square"></i></button>
+                            <button style={item.completed ? { backgroundColor: "green" } : {}} className="px-5 py-1 mx-1 border-2 m-auto hover:bg-green-600 " onClick={() => endTask(item)}>{item.completed ? "Completed" : "Complete"}</button>
                         </div>
                     </div>
                 </div>
